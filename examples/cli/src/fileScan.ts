a7895da3eda50abb9858bb4c5f6177be5a3a82dc
VitorCora <59590152+VitorCora@users.noreply.github.com>
2024-04-15 17:13:19 -0500

    Update fileScan.ts

--- examples/cli/src/fileScan.ts
+++ examples/cli/src/fileScan.ts
@@ -1,53 +1,39 @@
-import { AmaasGrpcClient, AmaasCredentials } from 'file-security-sdk'
-import loggerConfig from './loggerConfig'
+import { AmaasGrpcClient, AmaasCredentials } from 'file-security-sdk';
+import loggerConfig from './loggerConfig';
 
-/**
- * Handle environment variables.
- */
-const amaasHostName = process.env?.TM_AM_SERVER_ADDR ?? ''
-const key = process.env?.TM_AM_AUTH_KEY ?? ''
+const amaasHostName = process.env?.TM_AM_SERVER_ADDR ?? '';
+const key = process.env?.TM_AM_AUTH_KEY ?? '';
 const credent: AmaasCredentials = {
   credentType: process.env?.TM_AM_AUTH_KEY_TYPE === 'token' ? 'token' : 'apikey',
   secret: key
-}
-const useKey = false
+};
+const useKey = false;
 
-/**
- * runFileScan function
- *
- * Create a scan client instance using AmaasGrpcClient class.
- * Use AmaasGrpcClient.scanFile() to scan a file.
- *
- * @param fileName - Name of the file to scan
- * @param tags - List of string to tag a scan
- * @param pml - Enable predictive machine learning detection
- * @param feedback: Enable Trend Micro Smart Protection Network's Smart Feedback
- */
-const runFileScan = async (fileName: string, tags: string[], pml: boolean, feedback: boolean): Promise<void> => {
-  console.log(`\nScanning '${fileName}'`)
+const runFileScan = async (fileName, tags, pml, feedback) => {
+  console.log(`\nScanning '${fileName}'`);
   const amaasGrpcClient = useKey
     ? new AmaasGrpcClient(amaasHostName, key)
-    : new AmaasGrpcClient(amaasHostName, credent)
+    : new AmaasGrpcClient(amaasHostName, credent);
 
-  loggerConfig(amaasGrpcClient)
+  loggerConfig(amaasGrpcClient);
 
-  await amaasGrpcClient
-    .scanFile(fileName, tags, pml, feedback)
-    .then(result => {
-      console.log(`${JSON.stringify(result)}`)
-    })
-    .catch(err => {
-      throw err
-    })
-    .finally(() => {
-      amaasGrpcClient.close()
-    })
-}
+  try {
+    const result = await amaasGrpcClient.scanFile(fileName, tags, pml, feedback);
+    console.log(`${JSON.stringify(result)}`);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    amaasGrpcClient.close();
+  }
+};
 
-void (async () => {
-  // Examples of using AmaasGrpcClient with tags and TrendX with smart feedback
-  const tags = ['example', 'test']
-  const predictive_machine_learning = true
-  const smart_feedback = true
-  await runFileScan('node_modules/@grpc/grpc-js/src/admin.ts', tags, predictive_machine_learning, smart_feedback)
-})()
+// Extract command line arguments
+const [, , fileName, ...tags] = process.argv;
+
+// Examples of other parameters
+const predictive_machine_learning = true;
+const smart_feedback = true;
+
+runFileScan(fileName, tags, predictive_machine_learning, smart_feedback)
+  .then(() => console.log('File scan completed successfully'))
+  .catch(error => console.error('Error occurred during file scan:', error));
